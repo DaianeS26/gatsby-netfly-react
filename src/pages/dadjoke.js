4d5e2ef7d07b8f3b41cf5454c3387691d4cc020f
Daianes26 <daianeasilva26@gmail.com>
2020-04-20 17:29:50 -0400

    Dad joke

--- src/pages/dadjoke.js
+++ src/pages/dadjoke.js
@@ -1,43 +1,31 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const DadJokePage = () => {
+  const [joke, setJoke] = useState("")
 
-    const [joke, setJoke] = useState('');
+  const getDadJoke = () => {
+    fetch("/.netlify/functions/dad-joke")
+      .then(res => (res && res.json()) || {})
+      .then(resJson => resJson.msg)
+      .then(joke => setJoke(joke))
+  }
 
-    const getDadJoke = () => {
-        fetch('https://icanhazdadjoke.com/', {
-            headers: {
-            'Accept': 'application/json'
-            }
-        }).then(
-            res => res && res.json() || {}
-        ).then(
-            resJson => resJson.joke
-        ).then(
-            joke => setJoke(joke)
-        )
-    };
+  useEffect(() => {
+    getDadJoke()
+  }, [])
 
-    useEffect(() => {
-        getDadJoke();
-    }, [])
-
-    return (
-        <Layout>
-            <SEO title="DadJokes" />
-            <h1>Hi Hungry, I am Dad!</h1>
-            <p>Best dad jokes in the west...</p>
-            <button onClick={() => getDadJoke()}>
-                Click for dad joke
-            </button>
-            <br/>
-            <br/>
-            <p> { joke }</p>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <SEO title="DadJokes" />
+      <h1>Hi hungry, I'm Dad</h1>
+      <p>Best dad jokes in the west</p>
+      <button onClick={() => getDadJoke()}>Click for a dad joke</button>
+      <p>{joke}</p>
+    </Layout>
+  )
 }
 
 export default DadJokePage
